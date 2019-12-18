fb0f025c0f4d7a974053d4bd2c02d7cb54ced8e6
gnanisiddartha <gnanisiddartha@gmail.com>
2019-12-18 18:59:43 +0530

    Update CalculatorTest.cs

--- gitjenkins.Tests/CalculatorTest.cs
+++ gitjenkins.Tests/CalculatorTest.cs
@@ -12,7 +12,7 @@ namespace gitjenkins.Tests
         [Test]
         public void TestMethod1()
         {
-            Assert.AreEqual(5, calc.sum(2, 2));
+            Assert.AreEqual(4, calc.sum(2, 2));
         }
     }
 }
