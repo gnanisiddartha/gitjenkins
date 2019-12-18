dad511aa97d5c3bf7df1460bfe72673b57d0b152
gnanisiddartha <gnanisiddartha@gmail.com>
2019-12-18 18:29:20 +0530

    Add files via upload

---
+++ gitjenkins.Tests/CalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using gitjenkins.Models;
+
+namespace gitjenkins.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        Calculator calc = new Calculator();
+
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(5, calc.sum(2, 2));
+        }
+    }
+}
